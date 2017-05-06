0927d7e614ad93c98488c34697336d407ce97f7a
viktor <v.xhelilaj@gmail.com>
2017-05-06 02:42:22 -0400

    exercise 7 is simply more printing

---
+++ python_ex0-9/ex7.py
@@ -0,0 +1,22 @@
+print "Mary had a little lamb." # displays the string
+print "It's fleece was white as %s" % 'snow' # displays the string and uses the str placeholder to subsitute 'snow' in at the end of the string
+print "And everywhere that Mary went." # displays the string
+print "." * 10 # what'd that do?
+# displays the string 10 times ('string' * 10)
+
+end1 = "C" # stores the string as a variable
+end2 = "h" # stores the string as a variable
+end3 = "e" # stores the string as a variable
+end4 = "e" # stores the string as a variable
+end5 = "s" # stores the string as a variable
+end6 = "e" # stores the string as a variable
+end7 = "B" # stores the string as a variable
+end8 = "u" # stores the string as a variable
+end9 = "r" # stores the string as a variable
+end10 = "g" # stores the string as a variable
+end11 = "e" # stores the string as a variable
+end12 = "r" # stores the string as a variable
+
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, # displays the data types stored in the variables, the comma tells python to hold the pointer on the same line after each print and creates a space between the stored data types in the variables it separates
+print end7 + end8 + end9 + end10 + end11 + end12 # displays the data types stored in the variables
